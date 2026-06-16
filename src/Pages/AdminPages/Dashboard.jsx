825f5182c29780a086bce70019d2c86e689a941b
BigJoe85 <bigjooee4@gmail.com>
2026-06-16 14:14:24 -0400

    fixing styles breaking in product details section

--- src/Pages/AdminPages/Dashboard.jsx
+++ src/Pages/AdminPages/Dashboard.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
 const Dashboard = () => {
+  //date handler
+  const today = new Date().toLocaleDateString('en-US', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  })
+
   return (
     <div>
-      <h1 className='text-red-600'>Dashboard Here </h1>
+      <div>
+        <h1 className='text-red-600'>Dashboard Here </h1>
+        <p>{today}</p>
+      </div>
     </div>
   )
 }
